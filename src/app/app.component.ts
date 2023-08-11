1fd5634117047e7144b435e2dc417cea1e8991c3
Lautaro Garcia <lautarojgarcia177@gmail.com>
2023-08-11 20:11:14 -0300

    remover comentario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  // completar la funcion calcularSuma con todo el tipado correspondiente
   calcularSuma(numeros: number[]): number {
     let suma = 0;
     for (let numero of numeros) {
